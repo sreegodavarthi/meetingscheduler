d731cda8d11d50981f0d937afde857f8ac1cfb3a
Sree Godavarthi <sreeg32@gmail.com>
2017-06-19 11:58:07 -0400

    setup

---
+++ app.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+
+
+app.use('/js', express.static('./js'));
+app.use('/views', express.static('./views'));
+app.use('/css', express.static('./css'));
+
+
+app.listen(process.env.PORT || 7878, function() {
+    console.log('server listening at http://localhost:7878');
+});
+
+
+app.get('/', function(req, resp) {
+    resp.sendFile(__dirname + '/index.html');
+})
