b1cadde7d57f3a45b1d96680c0140f8fe3cf852d
Sree Godavarthi <sreeg32@gmail.com>
2017-06-20 10:53:12 -0400

    styled

--- js/app.js
+++ js/app.js
@@ -1,48 +1,47 @@
-var myApp = angular.module('myApp', 
-  ['ngRoute', 'firebase']);
+var myApp = angular.module('myApp', ['ngRoute', 'firebase']);
 
 myApp.run(['$rootScope', '$location', function($rootScope, $location) {
-  $rootScope.$on('$routeChangeError', function(event, next, previous, error) {
-    if (error == 'AUTH_REQUIRED') {
-      $rootScope.message = 'Sorry, you must log in to access that page';
-      $location.path('/login');
-    }//Auth Required
-  }); //$routeChangeError
+    $rootScope.$on('$routeChangeError', function(event, next, previous, error) {
+        if (error == 'AUTH_REQUIRED') {
+            $rootScope.message = 'Sorry, you must log in to access that page';
+            $location.path('/login');
+        } //Auth Required
+    }); //$routeChangeError
 }]); //run
 
 myApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
+    $routeProvider.
     when('/login', {
-      templateUrl: 'views/login.html',
-      controller: 'RegistrationController'
+        templateUrl: 'views/login.html',
+        controller: 'RegistrationController'
     }).
     when('/register', {
-      templateUrl: 'views/register.html',
-      controller: 'RegistrationController'
+        templateUrl: 'views/register.html',
+        controller: 'RegistrationController'
     }).
-    when('/report', {
-      templateUrl: 'views/report.html',
-      controller: 'ReportController'
+    when('/car-sales-report', {
+        templateUrl: 'views/report.html',
+        controller: 'ReportController'
     }).
     when('/checkins/:uId/:mId', {
-      templateUrl: 'views/checkins.html',
-      controller: 'CheckInsController'
+        templateUrl: 'views/checkins.html',
+        controller: 'CheckInsController'
     }).
     when('/checkins/:uId/:mId/checkinsList', {
-      templateUrl: 'views/checkinslist.html',
-      controller: 'CheckInsController'
+        templateUrl: 'views/checkinslist.html',
+        controller: 'CheckInsController'
     }).
-    
+
     when('/meetings', {
-      templateUrl: 'views/meetings.html',
-      controller: 'MeetingsController',
-      resolve: {
-        currentAuth: function(Authentication) {
-          return Authentication.requireAuth();
-        } //currentAuth
-      }//resolve
+        templateUrl: 'views/meetings.html',
+        controller: 'MeetingsController',
+        resolve: {
+            currentAuth: function(Authentication) {
+                    return Authentication.requireAuth();
+                } //currentAuth
+        } //resolve
     }).
     otherwise({
-      redirectTo: '/login'
+        redirectTo: '/login'
     });
 }]);
