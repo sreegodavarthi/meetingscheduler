179dfdb3bd7f76bcb35a645416791943998e1d4d
Sree Godavarthi <sreeg32@gmail.com>
2017-06-21 09:07:08 -0400

    refresh update

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.listen(process.env.PORT || 7878, function() {
 });
 
 
-app.get('/', function(req, resp) {
+app.get('/*', function(req, resp) {
     resp.sendFile(__dirname + '/index.html');
 })
